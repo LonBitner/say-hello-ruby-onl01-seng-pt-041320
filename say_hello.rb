eee024f7a8bb75db28b034046f3042aac4f0d594
Lon Bitner <lonbitner@gmail.com>
2020-04-15 21:12:55 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,3 +1,11 @@
 # Build your say_hello method here
 
-def 
+def introduction (name)
+  puts "Hi, my name is #{name}."
+  end
+ 
+ def introduction_with_language (name,language)
+   puts "Hi,my name is #{name} and i am studying #{language}."
+ end
+ 
+ introduction_with_language (name ="Josh",language ="Ruby"
